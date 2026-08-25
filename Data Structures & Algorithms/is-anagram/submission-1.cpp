2c42e9630f791ba1a97046cadc3cb00d72746484
harshita-079 <harshugupta0220@gmail.com>
2026-08-25 16:30:47 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!= t.size())return false;
+        unordered_map<char,int>mpp;
+        for(int i=0;i<s.size();i++){
+            mpp[s[i]]++;
+        }
+
+        for(int i=0;i<t.size();i++){
+            if(mpp.find(t[i])!=mpp.end()){
+                mpp[t[i]]--;
+                if(mpp[t[i]]==0)mpp.erase(t[i]);
+            }
+            else{
+                return false;
+            }
+        }
+        if(mpp.size()==0)return true;
+        return false;
+    }
+
+};
